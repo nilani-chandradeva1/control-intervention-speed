b581ccd3b6de5c1c95f209f7422014abe5791d61
nilani-chandradeva1 <nilani.chandradeva15@imperial.ac.uk>
2025-08-08 11:26:45 +0100

    new files

---
+++ 1.simple-seirs-sei/1.model-runs/1.seirs-sei-constant-emergence.R
@@ -0,0 +1,3 @@
+#simple SEIRS-SEI model for killing a target number of mosquitoes (delta_D) in a given time period
+#use optim to get best-fitting value of psi (additional mortality rate) to kill these mosquitoes (varies for each time period)
+#extreme density dependence (constant emergence)
