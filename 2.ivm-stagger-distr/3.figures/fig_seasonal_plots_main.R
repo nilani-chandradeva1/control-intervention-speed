0838db06486e7f2a5b49f6ed63d93125f68e0165
nilani-chandradeva1 <nilani.chandradeva15@imperial.ac.uk>
2025-11-26 10:47:49 +0000

    just need to fix the error bars

--- 2.ivm-stagger-distr/3.figures/fig_seasonal_plots_main.R
+++ 2.ivm-stagger-distr/3.figures/fig_seasonal_plots_main.R
@@ -1,3 +1,4 @@
+require(tidyverse)
 df_distr_seasonal <- readRDS("2.ivm-stagger-distr/output/df_distr_season_timed.rds")
 df_distr_seasonal_late <- readRDS("2.ivm-stagger-distr/output/df_distr_season_late.rds")
 
@@ -135,3 +136,109 @@ prev_plot_late <- ggplot(df_distr_seasonal_late, aes(x = (t - start)/365, y = sl
 dynamics <- cowplot::plot_grid(mv_plot_on_time, prev_plot_on_time,
                       mv_plot_late, prev_plot_late,
                       labels = c("A", "B", "C", "D"))
+
+
+#impact
+impact_on_time <- readRDS("2.ivm-stagger-distr/output/impact_seasonal_on_time.rds")
+covs_error_on_time <- readRDS("2.ivm-stagger-distr/output/impact_seasonal_covs_error.rds") %>%
+  mutate(seasonality = "seasonal-on-time"  )
+
+
+#impact-late
+impact_late <- readRDS("2.ivm-stagger-distr/output/impact_seasonal_late.rds")
+cov_error_late <- readRDS("2.ivm-stagger-distr/output/impact_seasonal_late_covs_error.rds")%>%
+  mutate(seasonality = "seasonal-on-late")
+
+
+impact_df <- rbind(impact_on_time, impact_late)
+covs_error_df <- rbind(covs_error_on_time, cov_error_late) %>%
+  mutate(scenario = case_when(scenario == "Once year since start" ~ "One_year_since_start",
+                              TRUE ~ scenario))
+
+impact_df2 <- impact_df %>%
+  filter(init_EIR == 100 & ivm_cov_par == 0.7) %>%
+  mutate(scenario = case_when(scenario == "Once year since start" ~ "One_year_since_start",
+                              TRUE ~ scenario))
+
+impact_df2$seasonality <- factor(impact_df2$seasonality, levels = c("seasonal-on-time", "seasonal-on-late"))
+
+require(ggpattern)
+
+
+distr_pals <- c('#66c2a5','#fc8d62','#8da0cb','#e78ac3','#a6d854')
+
+head(impa)
+
+
+ggplot()  +
+  geom_bar_pattern(data = impact_df2,
+                   aes(
+                     x = factor(scenario),
+                     y = impact,
+                     fill = as.factor(intervention),
+                     pattern = factor(seasonality,
+                                      levels = c("seasonal-on-time","seasonal-on-late"))
+                   ),
+    stat = "identity",
+    position = position_dodge2(preserve = "single", padding = 0.1),
+    color = "black",
+    pattern_colour = "black",
+    pattern_fill = NA,
+    pattern_density = 0.4,
+    pattern_spacing = 0.04,
+    pattern_key_scale_factor = 0.5
+  ) +
+  geom_errorbar(
+    data = covs_error_df,
+    aes(
+      x = factor(scenario),
+      ymin = cov_low_0.5,
+      ymax = cov_high_0.9,
+      fill = as.factor(intervention),
+      pattern = factor(seasonality,
+                       levels = c("seasonal-on-time","seasonal-on-late"))
+    ),
+    width = 0.2,
+    position = position_dodge2(preserve = "single", padding = 0.1),
+    size = 1.1
+  )
+  scale_fill_manual(
+    values = c("impact_all_in_stag" = distr_pals[3],
+               "impact_10d" = distr_pals[1],
+               "impact_20d" = distr_pals[2]),
+    breaks = c("impact_all_in_stag", "impact_10d", "impact_20d"),
+    labels = c(
+      "impact_all_in_stag"="1 day to complete MDA",
+      "impact_10d"="10 days to complete MDA",
+      "impact_20d"="20 days to complete MDA"
+    ),
+    name = "Scenario"
+  ) +
+  scale_pattern_manual(
+    values = c("seasonal-on-time"="none",
+               "seasonal-on-late"="stripe"),
+    breaks = c("seasonal-on-time","seasonal-on-late"),
+    labels = c("On time", "Late (by 2 months)"),
+    name = "Timing of MDA in seasonal setting"
+  ) +
+  guides(
+    fill = "none",
+    pattern = guide_legend(
+      override.aes = list(
+        fill = "white",
+        pattern_fill = "white",
+        pattern_colour = "black"
+      )
+    )
+  )+
+  theme_bw(base_size = 14)+
+  theme(legend.position = c(c(0.5, 0.9)))+
+  ylab("Efficacy (%)") +
+  xlab("Time of measurement") +
+  scale_x_discrete(labels = c(
+  "bohemia" = "Incidence U5s \n (start to 6m later)",
+  "matamal" = "All-age prevalence \n (1m after last MDA)",
+  "One_year_since_start" = "Incidence U5s \n (start to 1y later)"
+))
+
+
